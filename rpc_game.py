ae2558d3cfcf1b34ee262ca278d1ff051c4c7288
Shwetanshu13 <69039154+Shwetanshu13@users.noreply.github.com>
2020-10-02 12:34:19 +0530

    Add files via upload

--- rpc_game.py
+++ rpc_game.py
@@ -1,48 +1,78 @@
 import random
 
 
-def computer_choice():
-    choice_num = random.randint(1, 30)
-    if choice_num <= 10:
-        comp_choice = "rock"
-    elif choice_num <= 20:
-        comp_choice = "paper"
-    elif choice_num <= 30:
-        comp_choice = "scissor" 
-    return comp_choice
-
-
 if __name__ == "__main__":
-    comp_choice = computer_choice()
-    # print (comp_choice)
 
-    user_choice = input('''Enter your choice : "rock", "paper" or "scissor"\n''')
-    # print("\n")
+    points = int(input("How much point(s) game do you want to play : "))
 
-    if user_choice == comp_choice:
-        print ("It is a Tie\n")
+    comp_score = 0
+    user_score = 0
 
-    else:
-        if user_choice == "rock" and comp_choice == "paper":
-            print ("You Lose. Better Luck Next Time\n")
+    while 1:
 
-        elif user_choice == "paper" and comp_choice == "rock":
-            print ("Congratulatins! You Win\n")
+        if comp_score == points or user_score == points:
+            break
 
-        elif user_choice == "paper" and comp_choice == "scissor":
-            print ("You Lose. Better Luck Next Time\n")
+        choice_num = random.randint(1, 30)
 
-        elif user_choice == "scissor" and comp_choice == "paper":
-            print ("Congratulatins! You Win\n")
+        if choice_num <= 10:
+            comp_choice = "rock"
 
-        elif user_choice == "rock" and comp_choice == "scissor":
-            print ("Congratulatins! You Win\n")
+        elif choice_num <= 20:
+            comp_choice = "paper"
 
-        elif user_choice == "scissor" and comp_choice == "rock":
-            print ("You Lose. Better Luck Next Time\n")
+        elif choice_num <= 30:
+            comp_choice = "scissor"
 
+        # print (comp_choice)
 
+        user_choice = input(
+            '''Enter your choice : "rock", "paper" or "scissor"\n''')
+        # print("\n")
 
-    print ("Computer : " + comp_choice)
-    print ("User :" + user_choice)
+        if user_choice == comp_choice:
+            print("It is a Tie")
+
+        else:
+            if user_choice == "rock" and comp_choice == "paper":
+                print("You Lose. Better Luck Next Time")
+                comp_score += 1
+
+            elif user_choice == "paper" and comp_choice == "rock":
+                print("Congratulatins! You Win")
+                user_score += 1
+
+            elif user_choice == "paper" and comp_choice == "scissor":
+                print("You Lose. Better Luck Next Time")
+                comp_score += 1
+
+            elif user_choice == "scissor" and comp_choice == "paper":
+                print("Congratulatins! You Win")
+                user_score += 1
+
+            elif user_choice == "rock" and comp_choice == "scissor":
+                print("Congratulatins! You Win")
+                user_score += 1
+
+            elif user_choice == "scissor" and comp_choice == "rock":
+                print("You Lose. Better Luck Next Time")
+                comp_score += 1
+
+        print("Computer : " + comp_choice)
+        print("User :" + user_choice)
+
+        print("Computer : ") 
+        print(comp_score)
+        print("User :") 
+        print(user_score)
+
     
+    print("Computer :") 
+    print(comp_score)
+    print("User :") 
+    print(user_score)
+
+    if comp_score == points:
+        print("\nYou Lose. Better Luck Next Time\n")
+    else:
+        print("\nCongratulatins! You Win\n")
